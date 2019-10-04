c4280b30ccc0ca69a15f76c8591b8e09d33e2c2f
Jeff Daze <jdaze@hostopia.com>
2019-10-04 11:07:10 -0400

    Cleanup of main config values

--- client/src/main.js
+++ client/src/main.js
@@ -6,14 +6,14 @@ import store from './store'
 Vue.config.productionTip = false
 
 const config = {
-  apiKey: "AIzaSyACIlh81s06tr_ojPXHahSvkwMoD6j8mNA",
-    authDomain: "emojispin.firebaseapp.com",
-    databaseURL: "https://emojispin.firebaseio.com",
-    projectId: "emojispin",
-    storageBucket: "emojispin.appspot.com",
-    messagingSenderId: "111067851384",
-    appId: "1:111067851384:web:49aa95770647ba14085b8d",
-    measurementId: "G-F3NTCRYGBZ"
+  apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 }
 
 firebase.initializeApp(config)
